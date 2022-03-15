75b2acc6c4234cc898042d3d9663b7fa24e6fee0
pintubread <pintubreadboyle@gmail.com>
2022-03-15 10:54:26 +0800

    set up login, logout, register functions

--- kopidarat/urls.py
+++ kopidarat/urls.py
@@ -20,6 +20,9 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('test', views.test, name='test'),
-    path ("<str:name>",views.greet,name='greet')
+    
+    # AUTHENTICATION
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register")
 ]
--- kopidarat/views.py
+++ kopidarat/views.py
@@ -1,13 +1,83 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.db import connection
+from django.db import connection, IntegrityError
+from django.urls import reverse
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required, user_passes_test
+
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
-def test(request):
-    return HttpResponse("Test")
+def login_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("index"))
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            request.session["username"] = username
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "login.html")
 
-def greet(request,name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+
+def logout_view(request):
+    logout(request)
+    if "username" in request.session:
+        del request.session["username"]
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    context={}
+    status=''
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("index"))
+    if request.method == "POST":
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "register.html", {
+                "message": "Passwords must match."
+            })
+        # Attempt to create new user
+        # to do: modify that it directly inserts new user to database
+
+        ## Check if customerid is already in the table
+        with connection.cursor() as cursor:
+
+            cursor.execute("SELECT * FROM user WHERE email = %s", [request.POST['email']])
+            user = cursor.fetchone()
+
+            ## No user with same email
+            if user == None:
+                cursor.execute("SELECT * FROM user WHERE username = %s",[request.POST['username']])
+                user = cursor.fetchone()
+
+                if user == None:
+                    cursor.execute("INSERT INTO user VALUES (%s, %s, %s, %s, 'Member')"
+                            , [request.POST['full_name'], request.POST['username'], request.POST['email'],
+                            request.POST['password']])
+                    cursor.execute("INSERT INTO member VALUES %s", [request.POST['email']])
+                    return redirect('index')
+                else:
+                    status = 'Customer with username %s already exists' % (request.POST['username'])    
+            else:
+                status = 'Customer with email %s already exists' % (request.POST['email'])    
+    context['status'] = status
+    return render(request, "register.html", context)
+
+
+
+
