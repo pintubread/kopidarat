30f0b4b91bfa49c4771d964dbb7ba1ed49185709
pintubread <pintubreadboyle@gmail.com>
2022-03-31 14:58:53 +0800

    added not null constraints for foreign key

--- sql/ProceduresTriggers.sql
+++ sql/ProceduresTriggers.sql
@@ -95,24 +95,7 @@ ON review
 FOR EACH ROW 
 EXECUTE FUNCTION check_review_func();
 
----Dropping procedures and triggers
-DROP TRIGGER IF EXISTS check_review ON review;
-DROP TRIGGER IF EXISTS check_report ON report;
-DROP TRIGGER IF EXISTS check_capacity ON joins;
 
-
-DROP PROCEDURE create_new_activity(u_email VARCHAR,
-									u_activity_name VARCHAR,
-									u_category VARCHAR,
-									u_start_date_time TIMESTAMP,
-									u_venue VARCHAR,
-									u_capacity INTEGER);
-DROP FUNCTION check_report_func();
-DROP FUNCTION check_review_func();
-DROP FUNCTION check_capacity_func();
-DROP PROCEDURE add_new_member(full_name VARCHAR,username VARCHAR,
-							  email VARCHAR,phone_number VARCHAR,
-							  password VARCHAR);
 							  
 							  
 							  
