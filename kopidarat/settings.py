465e4379228a2c5cb03d657e5e498fde13fa6bac
pintubread <pintubreadboyle@gmail.com>
2022-03-31 15:42:18 +0800

    Update settings.py

--- kopidarat/settings.py
+++ kopidarat/settings.py
@@ -156,13 +156,5 @@ django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = ''
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
 
 django_heroku.settings(locals())
